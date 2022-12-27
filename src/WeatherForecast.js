4facf1acf3d67d60637b5d4b9f14c431eabeb79f
Maya <salkiewicz.maya@gmail.com>
2022-12-27 15:22:55 +0100

    next-days-forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,7 +17,17 @@ export default function WeatherForecast(props) {
   if (loaded) {
     return (
       <div className="WeatherForecast">
-        <WeatherForecastDay data={forecast[0]} />
+        <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
+        </div>
       </div>
     );
   } else {
